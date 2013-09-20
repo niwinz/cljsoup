8701272eb0d92e0ce2ef852fbe4eb71e92c4c296
Andrey Antukh <niwi@niwi.be>
2013-09-20 13:34:00 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject be.niwi/cjlsoup "0.1.0"
+(defproject be.niwi/cljsoup "0.1.0"
   :description "Clojure html parser based on jsoup."
-  :url "http://github.com/niwibe"
+  :url "http://github.com/niwibe/cljsoup"
   :license {:name "BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.5.1"]
